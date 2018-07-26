644b5e035663f3da0d25e37fa53182b38ca3eceb
Barbosa <bruna.barbosa@accenture.com>
2018-07-26 12:50:06 -0300

    middleware folder

--- server/models/user.js
+++ server/models/user.js
@@ -11,14 +11,14 @@ var UserSchema = new mongoose.Schema({
         minlength: 1,
         unique: true,
         validate: {
-            validator: validator.isEmail, 
+            validator: validator.isEmail,
             message: '{VALUE} is not a valid email'
         }
-    }, 
+    },
     password: {
         type: String,
-        required: true,
-        minlength: 6,
+        require: true,
+        minlength: 6
     },
     tokens: [{
         access: {
@@ -42,17 +42,32 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
 
-    user.tokens = user.tokens.concat([{access, token}]);
+    user.tokens.push({ access, token });
 
     return user.save().then(() => {
         return token;
     });
 };
 
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+};
+
 var User = mongoose.model('User', UserSchema);
 
-module.exports = {
-    User
-};
+module.exports = { User }
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose')
 var {AulaPresencial} = require('./models/aulaPresencial');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -95,6 +96,10 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
